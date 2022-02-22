3ed9b23140be062ff05824a10e91369ea1ad83f4
PogromcaPapai <jakubdakowski@gmail.com>
2022-02-22 15:55:48 +0100

    debug

--- static/app.js
+++ static/app.js
@@ -1,15 +1,23 @@
 nunjucks.configure({ autoescape: true });
 
+function form_to_dict(form) {
+    var d = {};
+    var f = new FormData(form);
+    for (var element of f.keys())
+        d[element] = f.get(element);
+    return d;
+}
+
 $(function() {
     $("a").click(function(e) {
-        render($(this).attr('href'), {});
         e.preventDefault();
+        render($(this).attr('href'), {});
     });
     $("form").submit(function(e) {
-        kwargs = (new FormData(e.target)).entries;
-        console.log(kwargs);
-        render($(this).attr('action'), kwargs);
         e.preventDefault();
+        kwargs = form_to_dict(e.target);
+        // console.log(kwargs);
+        render($(this).attr('action'), kwargs);
     });
 });
 
@@ -18,8 +26,8 @@ function render(href, kwargs)
     $.ajax({
         url: `views/${href}.html`,
         success: function (data) {
-            str = nunjucks.renderString(data, kwargs)
-            $("body").html(str)
+            str = nunjucks.renderString(data, kwargs);
+            $("body").html(str);
         },
     })
 }
